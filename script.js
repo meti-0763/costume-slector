9c2240d4e0f9054399ac28efae86ad2c2759014e
meti-0763 <daramade4@gmail.com>
2024-12-29 13:09:50 +0330

    eeeeeend

--- script.js
+++ script.js
@@ -11,46 +11,45 @@ let lis = document.querySelector(".options")
 let input = document.querySelector("#input")
 let ct = document.querySelector("#CountryT")
 
-
 let cuntv = ""
-elemG(countries)
+
 
 function tap (d){
-    cuntv = d.target.innerText
-
+    cuntv = d.target.innerText 
+    
     ct.innerText=d.target.innerText
+    
+    select.classList.remove('active')
 
     let rbg = document.querySelectorAll(".pels")
-
+    
     rbg.forEach((e)=>{
         e.style = "background:"
     })
     d.target.style = "background:#d9c7c7"
 
- }
-
-
+    input.value=""
     
+}
+
+
+
 
 function elemG (arr){
     
     lis.innerHTML=""
     
     arr.forEach(element => {
-
         
-    
         let pel = `<p class="pels" onclick="tap(event)">${element}</p>`
-  
+        
         lis.insertAdjacentHTML("afterbegin", pel);
-
+        
         if(element==cuntv){
             let ddp =  document.querySelector(".pels")
             
             console.log(ddp);
-            ddp.style = "background:#d9c7c7"
-            
-            
+            ddp.style = "background:#d9c7c7"  
         }
         
     });
@@ -70,6 +69,6 @@ input.addEventListener("input", (e)=>{
     
     elemG(newd)
     
-    
-    
 })
+
+elemG(countries)
